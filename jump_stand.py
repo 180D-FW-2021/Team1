a947953a98404011b89876ed8d872654379703a8
Isaac Xu <playerxu@gmail.com>
2021-11-17 16:19:40 -0800

    added a jump_stand code

--- jump_stand.py
+++ jump_stand.py
@@ -53,68 +53,67 @@ def detectPose(image, pose, draw=False, display=False):
         # Return the output image and the results of pose landmarks detection.
         return output_image, results
 
-def checkHandsJoined(image, results, draw=False, display=False):
+def checkLeftRight(image, results, draw=False, display=False):
     '''
-    This function checks whether the hands of the person are joined or not in an image.
+    This function finds the horizontal position (left, center, right) of the person in an image.
     Args:
-        image:   The input image with a prominent person whose hands status (joined or not) needs to be classified.
+        image:   The input image with a prominent person whose the horizontal position needs to be found.
         results: The output of the pose landmarks detection on the input image.
-        draw:    A boolean value that is if set to true the function writes the hands status &amp; distance on the output image. 
+        draw:    A boolean value that is if set to true the function writes the horizontal position on the output image. 
         display: A boolean value that is if set to true the function displays the resultant image and returns nothing.
     Returns:
-        output_image: The same input image but with the classified hands status written, if it was specified.
-        hand_status:  The classified status of the hands whether they are joined or not.
+        output_image:         The same input image but with the horizontal position written, if it was specified.
+        horizontal_position:  The horizontal position (left, center, right) of the person in the input image.
     '''
     
-    # Get the height and width of the input image.
+    # Declare a variable to store the horizontal position (left, center, right) of the person.
+    horizontal_position = None
+    
+    # Get the height and width of the image.
     height, width, _ = image.shape
     
-    # Create a copy of the input image to write the hands status label on.
+    # Create a copy of the input image to write the horizontal position on.
     output_image = image.copy()
     
-    # Get the left wrist landmark x and y coordinates.
-    left_wrist_landmark = (results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x * width,
-                          results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y * height)
- 
-    # Get the right wrist landmark x and y coordinates.
-    right_wrist_landmark = (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x * width,
-                           results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y * height)
+    # Retreive the x-coordinate of the left shoulder landmark.
+    left_x = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x * width)
+
+    # Retreive the x-corrdinate of the right shoulder landmark.
+    right_x = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x * width)
     
-    # Calculate the euclidean distance between the left and right wrist.
-    euclidean_distance = int(hypot(left_wrist_landmark[0] - right_wrist_landmark[0],
-                                   left_wrist_landmark[1] - right_wrist_landmark[1]))
+    # Check if the person is at left that is when both shoulder landmarks x-corrdinates
+    # are less than or equal to the x-corrdinate of the center of the image.
+    if (right_x <= width//2 and left_x <= width//2):
+        
+        # Set the person's position to left.
+        horizontal_position = 'Left'
+
+    # Check if the person is at right that is when both shoulder landmarks x-corrdinates
+    # are greater than or equal to the x-corrdinate of the center of the image.
+    elif (right_x >= width//2 and left_x >= width//2):
+        
+        # Set the person's position to right.
+        horizontal_position = 'Right'
     
-    # Compare the distance between the wrists with a appropriate threshold to check if both hands are joined.
-    if euclidean_distance < 130:
+    # Check if the person is at center that is when right shoulder landmark x-corrdinate is greater than or equal to
+    # and left shoulder landmark x-corrdinate is less than or equal to the x-corrdinate of the center of the image.
+    elif (right_x >= width//2 and left_x >= width//2):
         
-        # Set the hands status to joined.
-        hand_status = 'Hands Joined'
+        # Set the person's position to center.
+        horizontal_position = 'Center'
         
-        # Set the color value to green.
-        color = (0, 255, 0)
-        
-    # Otherwise.    
-    else:
-        
-        # Set the hands status to not joined.
-        hand_status = 'Hands Not Joined'
-        
-        # Set the color value to red.
-        color = (0, 0, 255)
-        
-    # Check if the Hands Joined status and hands distance are specified to be written on the output image.
+    # Check if the person's horizontal position and a line at the center of the image is specified to be drawn.
     if draw:
- 
-        # Write the classified hands status on the image. 
-        cv2.putText(output_image, hand_status, (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, color, 3)
+
+        # Write the horizontal position of the person on the image. 
+        cv2.putText(output_image, horizontal_position, (5, height - 10), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
         
-        # Write the the distance between the wrists on the image. 
-        cv2.putText(output_image, f'Distance: {euclidean_distance}', (10, 70),
-                    cv2.FONT_HERSHEY_PLAIN, 2, color, 3)
+        # Draw a line at the center of the image.
+        cv2.line(output_image, (width//2, 0), (width//2, height), (255, 255, 255), 2)
         
     # Check if the output image is specified to be displayed.
     if display:
- 
+
         # Display the output image.
         plt.figure(figsize=[10,10])
         plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
@@ -122,8 +121,8 @@ def checkHandsJoined(image, results, draw=False, display=False):
     # Otherwise
     else:
     
-        # Return the output image and the classified hands status indicating whether the hands are joined or not.
-        return output_image, hand_status
+        # Return the output image and the person's horizontal position.
+        return output_image, horizontal_position
 
 # Initialize mediapipe pose class.
 mp_pose = mp.solutions.pose
@@ -139,13 +138,13 @@ pose_video = mp_pose.Pose(static_image_mode=False, model_complexity=1, min_detec
 mp_drawing = mp.solutions.drawing_utils 
 
 # Initialize the VideoCapture object to read from the webcam.
-camera_video = cv2.VideoCapture(0)
+camera_video = cv2.VideoCapture(1)
 camera_video.set(3,1280)
 camera_video.set(4,960)
- 
+
 # Create named window for resizing purposes.
-cv2.namedWindow('Hands Joined?', cv2.WINDOW_NORMAL)
- 
+cv2.namedWindow('Horizontal Movements', cv2.WINDOW_NORMAL)
+
 # Iterate until the webcam is accessed successfully.
 while camera_video.isOpened():
     
@@ -168,19 +167,19 @@ while camera_video.isOpened():
     # Check if the pose landmarks in the frame are detected.
     if results.pose_landmarks:
             
-        # Check if the left and right hands are joined.
-        frame, _ = checkHandsJoined(frame, results, draw=True)
+        # Check the horizontal position of the person in the frame.
+        frame, _ = checkLeftRight(frame, results, draw=True)
                 
     # Display the frame.
-    cv2.imshow('Hands Joined?', frame)
+    cv2.imshow('Horizontal Movements', frame)
     
-    # Wait for 1ms. If a key is pressed, retreive the ASCII code of the key.
-    k = cv2.waitKey(1)
+    # Wait for 1ms. If a a key is pressed, retreive the ASCII code of the key.
+    k = cv2.waitKey(1) & 0xFF
     
     # Check if 'ESC' is pressed and break the loop.
     if(k == 27):
         break
- 
+
 # Release the VideoCapture Object and close the windows.
 camera_video.release()
 cv2.destroyAllWindows()
