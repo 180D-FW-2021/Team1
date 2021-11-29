7a84d843981cf01d52f0779300ba30dc097dccc3
Isaac Xu <playerxu@gmail.com>
2021-11-29 15:22:47 -0800

    Update together.py

--- pose_detection_code/together.py
+++ pose_detection_code/together.py
@@ -31,8 +31,62 @@ def detectPose(image, pose, draw=False, display=True):
     Args:  
         image: The input image with a prominent person whose pose landmarks needs to be detected.
         pose: The pose setup function required to perform the pose detection.
+        draw: a boolean value that if set to true the funciton draw pose landmakrs on the output image
+        display: a boolean value that is if set to true the function displays the original input image, and
+                    the resultant image and returns nothing
+    Returns:
+        output_image: the input image with the detect pose landmarks drawn if it was specified
+        results: the output of the pose landmarks detection on the input image
     '''
-    print("bruh")
+
+    #create a copy of the input image
+    output_image = image.copy()
+
+    #convert the image from BGR into RGB format
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    #perform pose detection
+    results = pose.process(imageRGB)
+
+    #retrieve the height and width of the input image
+    height, width, _ = image.shape
+
+    #initialize a list to store the detected landmarks
+    landmarks = []
+
+    #check if any landmarks are detected
+    if results.pose_landmarks:
+
+        #iterate over the detected landmarks
+        for landmark in results.pose_landmarks.landmark:
+
+            #append the landmark into the list
+            #it has already done the modification that multiplies it by the height and width of the image
+            landmarks.append((int(landmark.x * width), int(landmark.y * height)))
+
+        #check if the landmarks are specified to be drawn
+        if draw:
+            #draw pose landmarks on the output image
+            mp_drawing.draw_landmarks(image=output_image, landmark_list=results.pose_landmarks,
+                                    connections=mp_pose.POSE_CONNECTIONS,
+                                    landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255,255,255),
+                                                                                thickness=3, circle_radius=3),
+                                    connection_drawing_spec=mp_drawing.DrawingSpec(color=(49,125,237),
+                                                                                thickness=2, circle_radius=2))
+
+    # Check if the original input image and the resultant image are specified to be displayed.
+    if display:
+    
+        # Display the original input image and the resultant image.
+        plt.figure(figsize=[22,22])
+        plt.subplot(121);plt.imshow(image[:,:,::-1]);plt.title("Original Image");plt.axis('off');
+        plt.subplot(122);plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
+
+    #otherwise
+    else:
+            
+        #return the output image and the found landmarks
+        return output_image, landmarks
 
 def calculateAngle(landmark1, landmark2, landmark3):
     '''
@@ -62,6 +116,35 @@ def calculateAngle(landmark1, landmark2, landmark3):
     
     # Return the calculated angle.
     return angle
+
+def checkpose(landmarks, output_image, display=False):
+    '''
+    This function classifies the pose of the person depending on body angles and distance differences
+    There will be a hierarchy of poses, i.e what pose to detect first
+    Args:
+        landmarks: a list of detected landmarks of the person whose pose needs to be detected
+        output_image: a image of the person with the detected pose landmarks drawn
+        display: a boolean value that is if set to true the function displays the resultant image with the pose label
+        written on it and returns nothing
+    Returns:
+        output_image: the image with the detected pose landmarks drawn and pose label written
+        label: the classified pose label of the person in the output_image
+    '''
+
+    #initialize the label of the pose, which is unknown at this stage
+    label = 'Unknown Pose'
+
+    #specify the color (red) which is used for unknown pose
+    color = (0,0,255)
+
+    #here we will calculate the required angles
+
+    #here we will calculate the distance between two landmarks
+
+    #here we will check for what pose it could be
+
+    #here we will check if the pose was identified or not, and output the image
+    print("hyello")
 
 # Initialize the VideoCapture object to read from the webcam.
 camera_video = cv2.VideoCapture(0)
