1708580e5d8b99024202454b4e54acd6a3bd7db1
Isaac Xu <playerxu@gmail.com>
2022-02-21 13:37:31 -0800

    added buttond
    added additional buttons to separate detections

---
+++ total_install_script.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+apt-get update
+yes | pip install opencv-python
+yes | pip install mediapipe
+yes | pip install pyaudio
+yes | pip install six
+yes | pip install google-cloud-speech
+yes | pip install python-tk
+yes | pip install threading
+yes | pip install pillow
+yes | pip install imageio
+yes | pip install playsound
+yes | brew install portaudio --HEAD
+yes | pip install paho-mqtt
+yes | pip install imageio-ffmpeg
